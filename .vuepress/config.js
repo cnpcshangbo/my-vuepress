b85dc8c65e4c66f1dbe946514edfefda838020ce
Bo Shang <cnpcshangbo@gmail.com>
2021-10-18 13:17:00 -0500

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,8 +2,5 @@ module.exports = {
 	base: '/github-pages-vuepress/',
 	title: 'VuePress in Github Pages',
 	description: 'Static website built with VuePress and deployed on Github Pages',
-	dest: './docs',
-	head : [
-  ["script", {src : "https://cdn.auth0.com/js/auth0-spa-js/1.13/auth0-spa-js.production.js"}]
-]
+	dest: './docs'
 };
